9b6e998e73b0842a2189949c1ffe78d6ccb48e43
Jp1200 <jprkr96@gmail.com>
2021-07-12 00:14:35 -0500

    background added

---
+++ src/comps/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HeaderStyle.css";
+function Header(){
+    return(
+    <div className="header">
+        <header>
+            <div><span><p>Contact</p></span><span><p>Brian Holmes</p></span></div>
+        </header>
+        
+    </div>)
+}
+export default Header;
---
+++ src/comps/Main/Mainpage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Header from "../Header/Header"
+const Mainpage = () => {
+
+    return(
+        <div className="mainpage">
+            <Header/>
+        </div>
+    )
+};
+export default Mainpage;

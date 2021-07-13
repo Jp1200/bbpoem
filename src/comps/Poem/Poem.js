56bc9ce5c37aff0efeb4422f480189e834a027be
Jp1200 <jprkr96@gmail.com>
2021-07-13 08:18:05 -0500

    Working on opening animation

---
+++ src/comps/Poem/Poem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Poem(){
+    return(
+        <div>
+            <h1>
+                Beauty and Grace
+            </h1>
+            <p className="subtitle"> By Brian Holmes</p>
+        </div>
+    )
+}
